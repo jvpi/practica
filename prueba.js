37fbb74e663e537a946c7cdb31ddbf77b23b6d9c
Jess <jvpi189@gmail.com>
2022-06-20 21:39:59 -0400

    first commit

---
+++ prueba.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',function(req,res) {
+	res.send('<p>some html</p>')
+})
+app.listen(port, function() {
+	console.log('servidor')
+})
