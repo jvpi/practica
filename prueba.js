176ed086d9045c5c919177748f8ffc96401b4d10
Jess <jvpi189@gmail.com>
2022-07-06 18:51:55 -0400

    4 commit

--- prueba.js
+++ prueba.js
@@ -10,6 +10,6 @@ app.get('/',function(req,res) {
 	res.sendFile(path.join(__dirname,'prueba.html'))
 })
 app.use(express.static(path.join(__dirname)))
-console.log(path.join(__dirname))
+
 
 app.listen(port)
